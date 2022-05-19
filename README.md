f1d12d9afa643576ce6a93fd297b5c03023a4718
Joe Amato <jamato211@gmail.com>
2022-05-18 22:14:18 -0400

    docker additions

---
+++ README.md
@@ -0,0 +1,14 @@
+Introduction
+------------
+This repository is used to track past dives. Expected stats to log - Depth - Total time - Total gas used - Exposure suit type - Air Temp - Water Temp - Cylinder type - Weight - Dive guide - Dive buddies - Tags - General Notes section - Dive location
+
+Technology Used
+---------------
+
+
+Installation
+------------
+Download postgres-42.3.5-jbdc.jar and add it to the class path
+    https://jdbc.postgresql.org/download.html
+
+
---
+++ docker/create_tables.sql
@@ -0,0 +1,29 @@
+-- Table Dive
+
+CREATE EXTENSION pgcrypto;
+
+CREATE TABLE dive (
+	number 			SERIAL PRIMARY KEY,
+	location		TEXT,
+	exposure_suit		TEXT,
+	air_temp		DECIMAL,
+	water_temp		DECIMAL,
+	weight			DECIMAL,
+	guide			TEXT,
+	start_gas_pressure	INT,
+	end_gas_pressure	INT,
+	gas_type		TEXT
+);
+
+CREATE TABLE dive_group (
+	dive_numbers 		INT [],
+	name			TEXT,
+	PRIMARY KEY (dive_numbers, name)
+);
+
+CREATE TABLE users (
+	id		SERIAL PRIMARY KEY,
+	name		TEXT NOT NULL UNIQUE,
+	email 		TEXT NOT NULL UNIQUE,
+	password	TEXT NOT NULL
+);
